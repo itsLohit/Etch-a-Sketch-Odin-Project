e3830b7a8aed42b7cd54c81dc204b565c7fe8d81
itsLohit <109834876+itsLohit@users.noreply.github.com>
2024-07-16 21:24:08 +0530

    Created HTML CSS JAVASCRIPT FILES
    DOM manipulation used for dynamic grid creation
    Hover effect made using eventListner

---
+++ index.js
@@ -0,0 +1,68 @@
+// DOM Objects
+const sketchArea = document.querySelector("#sketchArea");
+const btnGridSize = document.querySelector("#btnGridSize");
+const btnReset = document.querySelector("#btnReset");
+
+
+sketchArea.style.width = "500px";
+sketchArea.style.height = "500px";
+sketchArea.style.border = "1px solid red";
+
+
+btnGridSize.addEventListener("click", promptGridSize);
+btnReset.addEventListener("click", resetGrid);
+
+function resetGrid(){
+    createGrid();
+}
+
+function promptGridSize(){
+    let userInput = prompt("Enter a number between 1 and 100");
+    if((userInput<1) || (isNaN(userInput))){
+        userInput = 1;
+    }
+    else if(userInput>100){
+        userInput = 100;
+    }
+    createGrid(userInput);
+}
+
+
+function changeBackgroundColor(){
+    this.style.backgroundColor = "#FACD3D";
+}
+
+// Creating A Grid
+function createGrid(size=16){
+    sketchArea.innerHTML = "";
+    for(let i=0; i<(size**2); i++){
+        const grid = document.createElement("div");
+        const gridSize = 500
+        /size;
+        grid.classList.add("grid");
+        grid.style.height = `${gridSize - 2}px`;
+        grid.style.width = `${gridSize - 2}px`;
+        grid.style.border = "1px solid black"
+        sketchArea.appendChild(grid);
+        grid.addEventListener("mouseover", changeBackgroundColor);
+        
+        console.log(size);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+createGrid();
